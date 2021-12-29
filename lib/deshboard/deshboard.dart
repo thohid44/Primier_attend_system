4e34b395e4ee5b81f4c7d9c98084a8b47e397bdf
thohid44 <thohidislam44@gmail.com>
2021-12-29 21:46:26 +0600

    select session Dropdown menu

--- lib/deshboard/deshboard.dart
+++ lib/deshboard/deshboard.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,7 @@ class Deshboard extends StatefulWidget {
 class _DeshboardState extends State<Deshboard> {
   final items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
   String? value;
+  String? value1;
 
   final TextEditingController _session_type = TextEditingController();
 
@@ -36,6 +39,11 @@ class _DeshboardState extends State<Deshboard> {
                 ),
                 child: DropdownButton<String>(
                   value: value,
+                  hint: const Text("Select Session Type"),
+                  style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
                   iconSize: 36,
                   icon: const Icon(
                     Icons.arrow_drop_down,
@@ -56,7 +64,12 @@ class _DeshboardState extends State<Deshboard> {
                   borderRadius: BorderRadius.circular(12),
                 ),
                 child: DropdownButton<String>(
-                  value: value,
+                  value: value1,
+                  hint: const Text("Select a Session "),
+                  style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold),
                   iconSize: 36,
                   icon: const Icon(
                     Icons.arrow_drop_down,
@@ -64,50 +77,8 @@ class _DeshboardState extends State<Deshboard> {
                   ),
                   isExpanded: true,
                   items: items.map(buildMenuItem).toList(),
-                  onChanged: (value) => setState(() {
-                    this.value = value;
-                  }),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 3, color: Colors.deepPurple),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: DropdownButton<String>(
-                  value: value,
-                  iconSize: 36,
-                  icon: const Icon(
-                    Icons.arrow_drop_down,
-                    color: Colors.black,
-                  ),
-                  isExpanded: true,
-                  items: items.map(buildMenuItem).toList(),
-                  onChanged: (value) => setState(() {
-                    this.value = value;
-                  }),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 3, color: Colors.deepPurple),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: DropdownButton<String>(
-                  value: value,
-                  iconSize: 36,
-                  icon: const Icon(
-                    Icons.arrow_drop_down,
-                    color: Colors.black,
-                  ),
-                  isExpanded: true,
-                  items: items.map(buildMenuItem).toList(),
-                  onChanged: (value) => setState(() {
-                    this.value = value;
+                  onChanged: (value1) => setState(() {
+                    this.value1 = value1;
                   }),
                 ),
               ),
@@ -142,6 +113,12 @@ class _DeshboardState extends State<Deshboard> {
   }
 
   DropdownMenuItem<String> buildMenuItem(String item) => DropdownMenuItem(
+        value: item,
+        child: Text(item,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+      );
+
+  DropdownMenuItem<String> buildMenuItem1(String item) => DropdownMenuItem(
         value: item,
         child: Text(item,
             style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
